f9c8862af5afa4c9d9fc30d2d00a9e8f65060d44
alimohammad12345 <englishforever97@gmail.com>
2025-04-06 10:58:51 +0300

    Update navbar.js

--- components/navbar.js
+++ components/navbar.js
@@ -1,4 +1,5 @@
 // components/navbar.js (Modified for Taxi Dalarna Borlänge)
+import React, { useState } from 'react';
 import Link from "next/link";
 import {
   FaMapMarkerAlt,
@@ -9,7 +10,8 @@ import {
 import Image from "next/image";
 import Logo from "../public/logo.png"; // Assumed logo path
 
-const Navbar = () => { // Changed component name to start with uppercase (standard practice)
+const Navbar = () => {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false); // Changed component name to start with uppercase (standard practice)
   return (
     <div>
       {/* Top Bar */}
@@ -63,29 +65,28 @@ const Navbar = () => { // Changed component name to start with uppercase (standa
             </a>
             {/* Mobile Menu Toggle */}
             <button
-              data-collapse-toggle="mobile-menu-4"
+          
               type="button"
               className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
               aria-controls="mobile-menu-4"
-              aria-expanded="false"
+              aria-expanded={isMobileMenuOpen}
+              onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
             >
               <span className="sr-only">Öppna huvudmeny</span>
               {/* Hamburger Icon */}
-              <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path></svg>
-              {/* Close Icon (Hidden by default) */}
-              <svg className="hidden w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
+              <svg className={`${!isMobileMenuOpen ? 'block' : 'hidden'} w-6 h-6`} fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path></svg>
+              {/* Close Icon */}
+              <svg className={`${isMobileMenuOpen ? 'block' : 'hidden'} w-6 h-6`} fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
             </button>
           </div>
           {/* Menu Links */}
           <div
-            className="hidden justify-between items-center w-full md:flex md:w-auto md:order-1"
-            id="mobile-menu-4" // Ensure this ID matches data-collapse-toggle
+            className={`${isMobileMenuOpen ? 'block' : 'hidden'} justify-between items-center w-full md:flex md:w-auto md:order-1`}
+            id="mobile-menu-4"
           >
-            {/* --- NEW MENU STRUCTURE --- */}
             <ul className="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 text-sm font-medium">
               <li>
                 <Link href="/">
-                  {/* Using NavLink structure for active state - adjust styling as needed */}
                   <a className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-amber-600 md:p-0">
                     HEM
                   </a>
@@ -119,7 +120,6 @@ const Navbar = () => { // Changed component name to start with uppercase (standa
                   </a>
                 </Link>
               </li>
-           
             </ul>
           </div>
         </div>
